61c676a380207c4ecb040bb5a731df66a8a1c2df
Mikhail <you@example.com>
2021-05-07 04:06:40 +0500

    update

--- index.js
+++ index.js
@@ -2,42 +2,54 @@ const puppeteer = require("puppeteer");
 const wait = require("./wait.js").wait;
 const log = console.log;
 const urls = [  
-                "https://twitter.com/RussianStandup/status/1371736631493980172",
-                //"https://twitter.com/Deni20893267/status/1371913663636983810",
-                //"https://twitter.com/RussianStandup/status/1379112039138918417"
+                // "https://twitter.com/RussianStandup",
+                // "https://twitter.com/RussianStandup/status/1371736631493980172",
+                // "https://twitter.com/Deni20893267/status/1371913663636983810",
+                // "https://twitter.com/RussianStandup/status/1379112039138918417"
+                "https://vk.com/nokita"
             ];
 
 async function main(){
-    const browser = await puppeteer.launch({defaultViewport:{width: 900, height:1300} });
+    const browser = await puppeteer.launch({headless:true, defaultViewport:{width: 900, height:800}/*product: "firefox",*/ });
     const page = await browser.newPage();
 
     for(let i =0; i< urls.length; i++){
 
         const url = urls[i];
 
-        await page.goto(url, {timeout: 0 , waitUntil: "domcontentloaded" });
+        await page.goto(url, {timeout: 0 , waitUntil: "load" });
         
-        let sec = 60;
-        log(`waiting ${sec} sec`);
-        await wait(sec);
+    //    let sec = 30;
+    //    log(`waiting ${sec} sec`);
+    //    await wait(sec);
 
-        await page.screenshot({ path: "./pictures/"+url.replace(/\W/g, "_")+".png" });
+        //await page.screenshot({ path: "./pictures/"+url.replace(/\W/g, "_")+".png" });
+        await page.pdf({printBackground: true, path: "./pdf/"+url.replace(/\W/g, "_")+".pdf" });
 
     }
 
 
-/*
-    for (let index = 0; index < 10000; index++) {
-
-        await wait(30);
-        await page.screenshot({ path: "./pictures/"+url.replace(/\W/g, "_")+index+".png" });
-        await page.mouse.wheel({deltaY: 200});
-
-    }  
-*/
     await browser.close();
+
     log("DONE !");
 }
 
-
 main();
+
+
+/*
+    for (let index = 0; index < 10000; index++) {
+        await wait(30);
+        await page.screenshot({ path: "./pictures/"+url.replace(/\W/g, "_")+index+".png" });
+        await page.mouse.wheel({deltaY: 200});
+    }  
+*/
+
+
+
+
+
+
+
+
+
